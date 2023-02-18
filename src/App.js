1dd0bdbddafbcfc6f9d8dea748002ae515e905b3
aakashrjrajpoot12 <aakashrjrajpoot@gmail.com>
2023-02-18 17:07:37 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+
+
+
+function MyComponent(){
+  return(<input value="Something"/>);
+}
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    //this.text=React.createRef();
+  }
+  render(){
+    console.log({this.refs.xxx.value});
+    return(
+      <>
+      <input ref="xxx"/>      { /*No this is not a prop*/}
+         
+         {/*  //we cant use like this as ,By-Default you may not use ref attribute on function based component.because,they do not have instances.  */}
+      </>
+    );
+  }
 }
