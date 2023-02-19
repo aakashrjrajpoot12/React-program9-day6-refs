868c72b70c8d72cd8be22564478762fbcf3e35ed
aakashrjrajpoot12 <aakashrjrajpoot@gmail.com>
2023-02-19 10:57:13 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
-
-
-
-function MyComponent(){
-  return(<input value="Something"/>);
+function MyComponent() {
+  return <input value="Something" />;
 }
-export default class App extends React.Component{
-  constructor(props){
+export default class App extends React.Component {
+  constructor(props) {
     super(props);
     //this.text=React.createRef();
   }
-  getValue = ()=>console.log(this.refs.xxx.value);
-  render(){
-   
-    return(
+  getValue = ()=> console.log(this.refs.xxx.value);
+  render() {
+    return (
       <>
-      <input ref= "xxx" onClick={this.getValue} />
-         {/*  //we cant use like this as ,By-Default you may not use ref attribute on function based component.because,they do not have instances.  */}
+        <MyComponent ref="xxx" onClick={this.getValue} />
+        {/*  //we cant use like this as ,By-Default you may not use ref attribute on function based component.because,they do not have instances.  */}
       </>
     );
   }
