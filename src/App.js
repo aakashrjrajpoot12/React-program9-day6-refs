dab919869d1395caf27f39e973ddc824b0fc9eb4
aakashrjrajpoot12 <aakashrjrajpoot@gmail.com>
2023-02-19 11:38:25 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -15,7 +15,28 @@ export default class App extends React.Component {
       <>
         <MyComponent ref="xxx" onClick={this.getValue} />
         {/*  //we cant use like this as ,By-Default you may not use ref attribute on function based component.because,they do not have instances.  */}
+       
       </>
     );
   }
 }
+
+
+/*
+
+
+import React, {useRef} from 'react';
+import './style.css';
+
+
+export default function App(props) {
+  const text_input=useRef(null);
+    return (
+      <>
+        <input type="text" ref={text_input}/>
+        
+      </>
+    );
+  }
+
+*/
